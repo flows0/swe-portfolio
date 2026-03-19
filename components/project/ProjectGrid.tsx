5805b32a38969006aee78c13da42cf05f88371fa
flowz0 <billylflowers@gmail.com>
2026-03-19 13:56:42 -0700

    Refactor ProjectGrid component to use a grid layout for ProjectCard elements, improving responsiveness and visual organization

--- components/project/ProjectGrid.tsx
+++ components/project/ProjectGrid.tsx
@@ -2,10 +2,12 @@ import ProjectCard from "./ProjectCard";
 
 export default function ProjectGrid() {
   return (
-    <div className="flex flex-col gap-y-4 mt-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
       <ProjectCard />
       <ProjectCard />
-      <ProjectCard />
+      <div className="md:col-span-2 md:w-1/2 md:mx-auto lg:col-span-1 lg:w-fit lg:mx-0">
+        <ProjectCard />
+      </div>
     </div>
   );
 }
