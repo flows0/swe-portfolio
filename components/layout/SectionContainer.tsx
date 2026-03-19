5dad51f61df9b2bf4803458f22051a3e522ed6f4
flowz0 <billylflowers@gmail.com>
2026-03-19 13:56:29 -0700

    Update SectionContainer component to set a maximum width and center align content, improving layout consistency and responsiveness

--- components/layout/SectionContainer.tsx
+++ components/layout/SectionContainer.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function SectionContainer({ children, id }: { children: ReactNode, id?: string }) {
   return (
-    <div id={id} className="mt-48 px-4 scroll-mt-48">
+    <div id={id} className="mt-48 max-w-7xl mx-auto px-4 scroll-mt-32">
       {children}
     </div>
   );
