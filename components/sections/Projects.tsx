36e9e2b29c28ced780637e83544da8a124e8fae9
flowz0 <billylflowers@gmail.com>
2026-03-19 14:41:29 -0700

    Update Projects component to change heading size from h5 to h4 for improved visual hierarchy

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
   return (
     <SectionContainer id="projects">
       <div>
-        <h2 className="text-brand950 text-h5 font-bold sm:text-h2">
+        <h2 className="text-brand950 text-h4 font-bold sm:text-h2">
           Featured Projects
         </h2>
         <p className="text-brand800 text-p mt-4 max-w-120">
