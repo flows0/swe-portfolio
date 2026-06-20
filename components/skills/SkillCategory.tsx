ec347937b77eeed1f52e40b73cb5b60f3f88dd94
bflows <billylflowers@gmail.com>
2026-06-19 18:25:25 -0700

    fix: update text color in SkillCategory component for consistency

--- components/skills/SkillCategory.tsx
+++ components/skills/SkillCategory.tsx
@@ -10,7 +10,7 @@ export default function SkillCategory({
   return (
     <div className="flex h-full min-h-0 flex-col rounded-2xl bg-brand200/10 sm:bg-transparent px-6 py-5 sm:px-0 sm:py-0">
       <div className="flex shrink-0 items-center gap-x-4">
-        <div style={{backgroundColor: color}} className="w-fit p-2 rounded-lg text-brand100">
+        <div style={{backgroundColor: color}} className="w-fit p-2 rounded-lg text-brand200">
           <Icon className="size-8" />
         </div>
         <h6 className="text-h6 font-bold text-brand950">{title}</h6>
